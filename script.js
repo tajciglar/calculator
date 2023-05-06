fbbb016e139769929c5a65cb98f4a62cc556ae1a
tajciglar <taj.brzovic@gmail.com>
2023-05-06 10:12:25 +0200

    Add calculation of multiple numbers in a operation

--- script.js
+++ script.js
@@ -1,84 +1,61 @@
 const container = document.querySelector('#container');
 let display = document.querySelector("#display");
 
-let temp = null;
 let operator = null;
-let flag = null;
-let number1 = "";
-let number2 = "";
+const number = [];
+
 // shows on display
 function showOnDisplay(value) {
 
     display.value += value;
-    if (value === '=') operate(number1,number2, operator);
+    // when '=' is clicked its calling the function operate
+    if (value === '=') {
+        display.value = operate(number);
+    }
+    if (isNaN(parseInt(value))) {
+        number.push(value);
+    } else {
+        const lastNumber = number[number.length-1];
 
-    if (flag === 1){
-        number2 += value;
-    }
-    else{
-        if (isNaN(parseInt(value)))
-        {
-            operator = value;
-            flag = 1;
-        }
-        else
-        {
-            number1 += value;
+        if (lastNumber && !isNaN(parseFloat(lastNumber))) {
+            number[number.length-1] += value;
+        } else {
+            number.push(value);
         }
     }
- 
 }
     
 
 // clears the display
-function clearDisplay(value) {
+function clearDisplay(value, result) {
     display.value = '';
+    const number = [];
+    
 }
 
-function add(n1, n2) {
-    display.value = n1 + n2;
-}
-function substract() {
-    display.value = n1 - n2;
-}
-function multiply() {
-    display.value = n1 * n2;
-}
-function divide() {
-    display.value = n1 / n2;
-}
+function operate(number) {
 
-function operate(number1, number2, operator) {
+    let result = parseInt(number[0]);
+    let currentOperator = null;
 
-    // converson from string to int
-    let n1 = parseInt(number1);
-    let n2 = parseInt(number2);
+    for (let i = 1; i < number.length; i++) {
+    const value = number[i];
 
-    if (operator == '+')
-    {
-        add(n1, n2);
+    if (!isNaN(parseInt(value))) {
+        const currentValue = parseInt(value);
+
+        if (currentOperator === '+') {
+        result += currentValue;
+        } else if (currentOperator === '-') {
+        result -= currentValue;
+        } else if (currentOperator === '*') {
+        result *= currentValue;
+        } else if (currentOperator === '/') {
+        result /= currentValue;
+        }
+    } else {
+        currentOperator = value;
     }
-    else if (operator == '-')
-    {
-        substract(n1, n2);
     }
-    else if (operator == '*')
-    {
-        multiply(n1, n2);
-    }
-    else if (operator == '/')
-    {
-        divide(n1, n2);
-    }
+    return(result)
 }
- /* let buttons = document.querySelectorAll("button");
-function calculate() {
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            do{
-
-            } while(button.classList.contains(sign));
-        });
-    });
-}
-*/
